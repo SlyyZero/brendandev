74a286946f4b4921aa56d611083cf013f9604594
Brendan <brendanjonsson.1@gmail.com>
2025-04-29 09:32:13 -0700

    updated to close the sidebar on open space touch

--- brendandev-portfolio/src/components/Shell.tsx
+++ brendandev-portfolio/src/components/Shell.tsx
@@ -7,13 +7,26 @@ import MobileNavToggle from '@/components/MobileNavToggle';
 export default function Shell({ children }: { children: React.ReactNode }) {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
+  const closeSidebar = () => setIsSidebarOpen(false);
+
   return (
-    <div className="flex flex-col md:flex-row">
+    <div className="relative flex flex-col md:flex-row min-h-screen">
+      {/* Mobile Hamburger Button */}
       <MobileNavToggle onToggle={() => setIsSidebarOpen(!isSidebarOpen)} />
+
+      {/* Sidebar */}
       <Sidebar isOpen={isSidebarOpen} />
-      <main className="flex-1 p-8">
-        {children}
-      </main>
+
+      {/* Overlay to detect clicks outside */}
+      {isSidebarOpen && (
+        <div
+          onClick={closeSidebar}
+          className="fixed inset-0 bg-black bg-opacity-30 md:hidden z-30"
+        ></div>
+      )}
+
+      {/* Main Content */}
+      <main className="flex-1 p-8 z-20">{children}</main>
     </div>
   );
 }
